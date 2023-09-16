9387a04b5dc70a3707bd6edc30d391138c9048ef
TBF <troflog@gmail.com>
2023-09-16 09:32:21 +0200

    Change into a repo for diverese dotfiles

--- vg.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import scipy as sc
-import matplotlib.pyplot as plt
-x = np.random.normal(size=4)
-y = sc.rand()
-plt.plot(x,x)
-#plt.show()
-print(y)
-g = sc.stats.norm.rvs(size = 31)
