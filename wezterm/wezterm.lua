ca0e297f9367a9f4f951c6b984504ba6107d63ab
flo <troflog@gmail.com>
2023-09-23 13:12:09 +0200

    Added better Wezterm split,using Alt+\ and Alt+-

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,8 +6,7 @@ local config = {}
 
 -- Text
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
+-- In newer versions of wezterm, use the config_builder which will help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
@@ -15,11 +14,22 @@ end
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 9 
+config.font_size = 9
 -- For example, changing the color scheme:
 config.color_scheme = 'tokyonight'
 -- Remove title bar
 config.window_decorations = "RESIZE"
-
+config.keys = {
+  {
+    key = "\\",
+    mods = "ALT",
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = "-",
+    mods = "ALT",
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  }
+}
 -- and finally, return the configuration to wezterm
 return config
