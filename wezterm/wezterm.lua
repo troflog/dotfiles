e775e15665b706aca54269603a9f1cb1a805ab1d
flo <troflog@gmail.com>
2023-10-14 18:14:09 +0200

    Added Nvim navigator to Wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -22,6 +22,37 @@ config.window_decorations = "RESIZE"
 -- tab_bar flase renders it in a retro aesthetic using the main terminal font.
 config.use_fancy_tab_bar= false
 
+
+----Setup Navigator.nvim----
+local w = require 'wezterm'
+local a = w.action
+
+local function is_inside_vim(pane)
+  local tty = pane:get_tty_name()
+  if tty == nil then return false end
+
+  local success, stdout, stderr = w.run_child_process
+    { 'sh', '-c',
+      'ps -o state= -o comm= -t' .. w.shell_quote_arg(tty) .. ' | ' ..
+      'grep -iqE \'^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?)(diff)?$\'' }
+
+  return success
+end
+
+local function is_outside_vim(pane) return not is_inside_vim(pane) end
+
+local function bind_if(cond, key, mods, action)
+  local function callback (win, pane)
+    if cond(pane) then
+      win:perform_action(action, pane)
+    else
+      win:perform_action(a.SendKey({key=key, mods=mods}), pane)
+    end
+  end
+
+  return {key=key, mods=mods, action=w.action_callback(callback)}
+end
+
 --Shortcuts
 config.keys = {
   {
@@ -33,7 +64,11 @@ config.keys = {
     key = "-",
     mods = "ALT",
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  }
+  },
+  --Nvim navigator
+    bind_if(is_outside_vim, 'h', 'CTRL', a.ActivatePaneDirection('Left')),
+    bind_if(is_outside_vim, 'l', 'CTRL', a.ActivatePaneDirection('Right')),
+    bind_if(is_outside_vim, 'j', 'CTRL', a.ActivatePaneDirection('Up')),
+    bind_if(is_outside_vim, 'k', 'CTRL', a.ActivatePaneDirection('Down')),
 }
--- and finally, return the configuration to wezterm
 return config
